95ad5ff95d7754f7be58b84bd4939a5f74b3cec0
lotar <lotar1982@outlook.com>
2021-10-19 13:37:06 +0200

    its works in class Card

--- pages/lang-components/card.tsx
+++ pages/lang-components/card.tsx
@@ -1,49 +1,84 @@
 //import Image from 'next/image'
 //import revers1 from "../../public/img/revers1.png";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 //import { ICard } from "./ICard";
 import CardValue from './CardValue';
 //import type { AppProps } from 'next/app'
 
 //function LangLayout({ Component, pageProps }: AppProps) {
-function Card({nRound,onecard,toggle,delay}:any) {
+//function Card({nRound,onecard,toggle,delay}:any) {
 
+interface CardItemsProps {
+    nRound:boolean;
+    startRound:any;
+    onecard:any;
+    toggle:any;
+    delay:number;
+};
+
+class Card extends React.Component<CardItemsProps,any>{
+
+    constructor(props:CardItemsProps){
+        super(props);
+        this.state = {
+            clickState:false
+        }
+    }
+    /*
     const [clickState,setClickState] = useState(false);
     ///console.log(...props);
     const press = ()=>{
        setClickState(true);
        toggle(onecard,clickState); //add clickState
       // e.target.children[0].className = "nativecard cardfront cardbox-hover pik";
-    };
+    };*/
+
+    press = ()=>{
+        this.setState({
+            clickState:true,
+        });
+        this.props.toggle(this.props.onecard,this.state.clickState);
+    }
 
 
-    useEffect(
-        ()=>{
-            if(nRound){
+    componentDidUpdate(prevProps:any, prevState:any){
+        console.log("prev: "+prevProps.nRound);
+            console.log("this: " + this.props.nRound);
+            if(this.props.nRound){
 
                 setTimeout(()=>{
-                    setClickState(false);
-                    toggle(onecard,clickState);
+                     this.setState({clickState:false});
+                     this.props.toggle(this.props.onecard,this.props.nRound);
+                     this.props.startRound();
+                     console.log('new round');
+                 },this.props.delay*100);
 
-                    console.log('new round');
-                },delay*100);
-
-            }
-
-
-        },[nRound]
-    );
+             }
+      //  }
 
 
+            console.log(this.props.nRound);
 
-    return  (
-        <div className="cardbox" onClick={press.bind(onecard)}>
-            <div className={clickState?"cardbox_click card":"card"}>
-                <div className={`nativecard cardfront ${onecard.color}`}>
-                    <CardValue valCard={onecard.name}/>
+    }
+
+
+
+    render(){
+
+        return  (
+            <div className="cardbox" onClick={this.press.bind(this.props.onecard)}>
+                <div className={this.state.clickState?"cardbox_click card":"card"}>
+                    <div className={`nativecard cardfront ${this.props.onecard.color}`}>
+                        <CardValue valCard={this.props.onecard.name}/>
+                    </div>
+                    <div className="nativecard cardback"></div>
                 </div>
-                <div className="nativecard cardback"></div>
-            </div>
-        </div>);
+            </div>);
+    }
+
+
 }
+
+
+
 export default Card;
--- pages/lang-components/lang-app.tsx
+++ pages/lang-components/lang-app.tsx
@@ -28,6 +28,10 @@ class LangApp extends React.Component<{},any>  {
     this.setState({playerPoints:+card.value});
   }
 
+  /**
+    * dewaluuje punkty z number na string
+    * dodaje do siebie l.dzies i l.jendosci i zwraca jako numbr
+  */
   devalueScore(scr:number){
     let sum:number = 0;
 
@@ -38,7 +42,6 @@ class LangApp extends React.Component<{},any>  {
 
       for(let i in b){
         let q = Number(b[i]);
-        //console.debug(q);
         c.push(q);
         sum = c.reduce((a,b)=>a+b);
       };
@@ -47,15 +50,16 @@ class LangApp extends React.Component<{},any>  {
     while(sum>9){
       try{change(sum);}catch(e){
         throw new Error('sorry there is somthing wrong');
-        console.debug(e);
-        break;
       }
     }
-
-    console.debug(sum);
     return sum;
   }
 
+  /**
+   *
+   * @param card liczy punkty playera
+   * @param clState
+   */
   Click_Player = (card:ICard,clState:boolean)=>{
     if(!clState){
       const sum = this.state.playerPoints + card.value;
@@ -63,6 +67,11 @@ class LangApp extends React.Component<{},any>  {
     //console.log(card.value);
   };
 
+  /**
+   *
+   * @param card liczy punkty krupiera
+   * @param clState
+   */
   Click_Croupier = (card:ICard,clState:boolean)=>{
     if(!clState){
       const sum = this.state.crupierPoints + card.value;
@@ -70,6 +79,11 @@ class LangApp extends React.Component<{},any>  {
     //console.log(card.value);
   };
 
+  /**
+   * funkcja przekazana do karty
+   * po wcisnieciu butta nowa runda
+   * ustawia state: nowa runda na true
+   */
   Click_NewRound = ()=>{
       this.setState({
         newRound:true,
@@ -77,6 +91,19 @@ class LangApp extends React.Component<{},any>  {
         crupierPoints:0,
 
       });
+  }
+
+/**
+  * funkcja przekazana do karty
+  * zmienia state:Nowa runda na false po obrocie kart
+  * aby dalo sie je znow obrocic
+*/
+  Set_NewRoundFalse = ()=>{
+    this.setState({
+      playerPoints:0,
+      crupierPoints:0,
+      newRound:false,
+    });
   }
 
   render(){
@@ -89,18 +116,18 @@ class LangApp extends React.Component<{},any>  {
             <div className="flex-center"><p className="margin-right name">Player:</p> <Coin pPoints={this.devalueScore(this.state.playerPoints)} addr={"img/zeton.svg"}wid={"3rem"} /></div>
 
               <div className="mainlang flex-center">
-                <Card key={"1"} nRound={this.state.newRound} onecard={this.sDesk[0]} toggle={this.Click_Player} delay={1}/>
-                <Card key={"2"} nRound={this.state.newRound} onecard={this.sDesk[1]} toggle={this.Click_Player} delay={2}/>
-                <Card key={"3"} nRound={this.state.newRound} onecard={this.sDesk[2]} toggle={this.Click_Player} delay={3}/>
+                <Card key={"1"} nRound={this.state.newRound} startRound = {this.Set_NewRoundFalse} onecard={this.sDesk[0]} toggle={this.Click_Player} delay={1}/>
+                <Card key={"2"} nRound={this.state.newRound} startRound = {this.Set_NewRoundFalse} onecard={this.sDesk[1]} toggle={this.Click_Player} delay={2}/>
+                <Card key={"3"} nRound={this.state.newRound} startRound = {this.Set_NewRoundFalse} onecard={this.sDesk[2]} toggle={this.Click_Player} delay={3}/>
               </div>
             </div>
 
             <div className="player">
               <div className="flex-center"><p className="margin-right name">Croupier:</p> <Coin pPoints={this.devalueScore(this.state.crupierPoints)} addr={"img/zeton.svg"}wid={"3rem"} /></div>
               <div className="mainlang flex-center">
-                <Card key={"4"} nRound={this.state.newRound} onecard={this.sDesk[3]} toggle={this.Click_Croupier} delay={4}/>
-                <Card key={"5"} nRound={this.state.newRound} onecard={this.sDesk[4]} toggle={this.Click_Croupier} delay={5}/>
-                <Card key={"6"} nRound={this.state.newRound} onecard={this.sDesk[5]} toggle={this.Click_Croupier} delay={6}/>
+                <Card key={"4"} nRound={this.state.newRound} startRound = {this.Set_NewRoundFalse} onecard={this.sDesk[3]} toggle={this.Click_Croupier} delay={4}/>
+                <Card key={"5"} nRound={this.state.newRound} startRound = {this.Set_NewRoundFalse} onecard={this.sDesk[4]} toggle={this.Click_Croupier} delay={5}/>
+                <Card key={"6"} nRound={this.state.newRound} startRound = {this.Set_NewRoundFalse} onecard={this.sDesk[5]} toggle={this.Click_Croupier} delay={6}/>
               </div>
             </div>
           </div>
